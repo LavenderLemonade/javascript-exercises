90430bb1f906f8912c1ed75e46dabfc5bff8d5a4
Sam Elliott <soe1@uakron.edu>
2022-10-21 12:40:20 -0400

    more work to do

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function tempConvertFtoC(temp) 
+{
+  return ((temp - 32) * (5/9));
 };
 
-const ctof = function() {
-
+const ctof =  function tempConvertCtoF(temp) 
+{
+  return ((temp * (9/5)) + 32)
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) 
+{
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) 
+{
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(stuff) 
+{
+	let holder;
+  for(i = 0; i < stuff.length; i++)
+  {
+    holder += stuff[i];
+  }
+
+  return holder;
+
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) 
+{
+  return num1 * num2;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) 
+{
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function() 
+{
+  
 };
 
 // Do not edit below this line
